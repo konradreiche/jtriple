7ca70ee5554a6bb9cf5b2df497c47702652d347b
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-08-02 01:47:01 +0200

    Exclude empty objects
    An object is empty if all its fields except the identifier
    field are null. I have added a check for empty objects to
    the object converter since they do not contain any relevant
    information for the resource to be associated with.
    
    Hence empty objects are not bound to the RDF model and not
    associated with the resource in a RDF statement.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- src/main/java/berlin/reiche/jtriple/converter/ObjectConverter.java
+++ src/main/java/berlin/reiche/jtriple/converter/ObjectConverter.java
@@ -1,6 +1,10 @@
 package berlin.reiche.jtriple.converter;
 
+import java.lang.reflect.Field;
+
 import berlin.reiche.jtriple.Binding;
+import berlin.reiche.jtriple.Util;
+import berlin.reiche.jtriple.rdf.Id;
 
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
@@ -18,16 +22,51 @@ public class ObjectConverter extends AbstractConverter {
     }
 
     @Override
-    public void convertEntity(Resource subject, Property predicate, Object object) throws Exception {
-        Resource nextResource = binding.createNewResource(object, object.getClass());
-        binding.bind(object);
-        subject.addProperty(predicate, nextResource);                 
+    public void convertEntity(Resource subject, Property predicate,
+            Object object) throws Exception {
+
+        if (!isEmpty(object)) {
+            Resource nextResource = binding.createNewResource(object,
+                    object.getClass());
+            binding.bind(object);
+            subject.addProperty(predicate, nextResource);
+        }
     }
 
-    
+    /**
+     * Sometimes the fields of an object are throughout <code>null</code>. If
+     * this is the case the object does not hold any information for the
+     * resource it should be associated with. This method checks whether this is
+     * the case.
+     * 
+     * @param object
+     *            the object to be checked for <code>null</code> fields.
+     * @return whether the object's fields are throughout <code>null</code>
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     */
+    private boolean isEmpty(Object object) throws IllegalArgumentException,
+            IllegalAccessException {
+
+        for (Field field : Util.getAllFields(object.getClass())) {
+
+            if (field.isAnnotationPresent(Id.class)) {
+                continue;
+            }
+
+            field.setAccessible(true);
+            Object value = field.get(object);
+            field.setAccessible(false);
+            if (value != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public boolean canConvert(Class<?> type, Object object) {
         return true;
     }
-    
+
 }
